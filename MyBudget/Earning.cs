42cae99488a592fdefc6321ccc7991d2f81b9f78
KolomoetsAA <favcr5@gmail.com>
2018-09-25 20:33:49 +0300

    created Earning & Spending

---
+++ MyBudget/Earning.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBudget
+{
+    class Earning
+    {
+        public Earning()
+        {
+
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
